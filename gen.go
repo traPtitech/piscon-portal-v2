106274dd983e7d8256d3d17e075dee75cdb4454e
pirosiki197 <lingmukangtai825@gmail.com>
2024-12-16 13:36:13 +0900

    コード生成用ファイルをserverに移動

--- gen.go
+++
@@ -1,4 +0,0 @@
-package portal
-
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@v1.8.1 -config ogen.yaml -package openapi -target server/openapi -clean openapi/openapi.yml
-//go:generate go run github.com/stephenafamo/bob/gen/bobgen-mysql@v0.29.0 -c bobgen.yaml
