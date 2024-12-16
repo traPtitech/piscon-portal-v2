b4f88089923fab2956ab6571f6c4379505ca7394
pirosiki197 <lingmukangtai825@gmail.com>
2024-12-16 13:20:07 +0900

    ogen で生成する package 変える runner用のgrpcのことも考えると、handler下よりも、別packageで生成したほうがよさそう

--- gen.go
+++ gen.go
@@ -1,4 +1,4 @@
 package portal
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@v1.8.1 -config ogen.yaml -package api -target server/handler/api -clean openapi/openapi.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@v1.8.1 -config ogen.yaml -package openapi -target server/openapi -clean openapi/openapi.yml
 //go:generate go run github.com/stephenafamo/bob/gen/bobgen-mysql@v0.29.0 -c bobgen.yaml
