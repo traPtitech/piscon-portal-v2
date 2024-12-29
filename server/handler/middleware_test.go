bc22396040708bfc808c8eed5f59eea051d42429
pirosiki <130675136+pirosiki197@users.noreply.github.com>
2024-12-29 18:19:27 +0900

    ci の設定 (#9)
    * ci の設定
    
    * gomock の Call はgoroutine safe ではなかった

--- server/handler/middleware_test.go
+++ server/handler/middleware_test.go
@@ -68,7 +68,6 @@ func TestAuthMiddleware(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
 			tt.setup()
 
 			req := httptest.NewRequest(http.MethodGet, "/", nil)
