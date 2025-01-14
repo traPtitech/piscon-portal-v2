5a50277a28cf6d532cacaad6b57457ff204f4e66
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-01-14 15:42:03 +0900

    :recycle: Transactionの処理を簡潔にする

--- server/repository/db/repository.go
+++ server/repository/db/repository.go
@@ -8,43 +8,51 @@ import (
 	"github.com/traPtitech/piscon-portal-v2/server/repository"
 )
 
+type repoDB struct {
+	// prevent direct access to bob.DB by beginning with an underscore
+	_db bob.DB
+}
+
+type executorCtxKeyT struct{}
+
+var executorCtxKey = executorCtxKeyT{}
+
+func (db *repoDB) executor(ctx context.Context) bob.Executor {
+	if v := ctx.Value(executorCtxKey); v != nil {
+		exe, ok := v.(bob.Executor)
+		if ok {
+			return exe
+		}
+	}
+
+	return db._db
+}
+
 type Repository struct {
-	db bob.DB
+	*repoDB
 }
 
 func NewRepository(db *sql.DB) *Repository {
 	return &Repository{
-		db: bob.NewDB(db),
-	}
-}
-
-type txRepository struct {
-	tx bob.Tx
-}
-
-func newTxRepository(tx bob.Tx) *txRepository {
-	return &txRepository{
-		tx: tx,
+		repoDB: &repoDB{
+			_db: bob.NewDB(db),
+		},
 	}
 }
 
 func (r *Repository) Transaction(ctx context.Context, f func(ctx context.Context, r repository.Repository) error) error {
-	tx, err := r.db.BeginTx(ctx, nil)
+	tx, err := r._db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
 	defer tx.Rollback() //nolint errcheck
 
-	txRepo := newTxRepository(tx)
+	ctx = context.WithValue(ctx, executorCtxKey, tx)
 
-	err = f(ctx, txRepo)
+	err = f(ctx, r)
 	if err != nil {
 		return err
 	}
 
 	return tx.Commit()
-}
-
-func (t *txRepository) Transaction(ctx context.Context, f func(ctx context.Context, r repository.Repository) error) error {
-	return f(ctx, t)
 }
--- server/repository/db/user.go
+++ server/repository/db/user.go
@@ -14,19 +14,11 @@ import (
 )
 
 func (r *Repository) FindUser(ctx context.Context, id string) (domain.User, error) {
-	return findUser(ctx, r.db, id)
+	return findUser(ctx, r.executor(ctx), id)
 }
 
 func (r *Repository) CreateUser(ctx context.Context, user domain.User) error {
-	return createUser(ctx, r.db, user)
-}
-
-func (t *txRepository) FindUser(ctx context.Context, id string) (domain.User, error) {
-	return findUser(ctx, t.tx, id)
-}
-
-func (t *txRepository) CreateUser(ctx context.Context, user domain.User) error {
-	return createUser(ctx, t.tx, user)
+	return createUser(ctx, r.executor(ctx), user)
 }
 
 func findUser(ctx context.Context, executor bob.Executor, id string) (domain.User, error) {
