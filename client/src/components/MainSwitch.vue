e1bf37b14d0c7b9320bcca7a898866cfee300f22
cp-20 <n.naoki.1587@gmail.com>
2025-01-19 01:21:43 +0900

    switch component

---
+++ client/src/components/MainSwitch.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { useId } from 'vue'
+
+const model = defineModel()
+const id = useId()
+</script>
+
+<template>
+  <div class="checkbox-wrapper">
+    <input :id="id" type="checkbox" class="switch" v-model="model" />
+    <label :for="id">
+      <slot />
+    </label>
+  </div>
+</template>
+
+<style>
+.checkbox-wrapper {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.checkbox-wrapper .switch {
+  appearance: none;
+  background-color: var(--ct-slate-300);
+  border-radius: 72px;
+  border-style: none;
+  flex-shrink: 0;
+  height: 20px;
+  margin: 0;
+  position: relative;
+  width: 30px;
+  cursor: default;
+}
+
+.checkbox-wrapper .switch::before {
+  bottom: -6px;
+  content: '';
+  left: -6px;
+  position: absolute;
+  right: -6px;
+  top: -6px;
+}
+
+.checkbox-wrapper .switch,
+.checkbox-wrapper .switch::after {
+  transition: all 200ms ease;
+}
+
+.checkbox-wrapper .switch::after {
+  background-color: #fff;
+  border-radius: 50%;
+  content: '';
+  height: 14px;
+  left: 3px;
+  position: absolute;
+  top: 3px;
+  width: 14px;
+}
+
+.checkbox-wrapper .switch:hover {
+  background-color: var(--ct-slate-400);
+  transition-duration: 0s;
+}
+
+.checkbox-wrapper .switch:checked {
+  background-color: var(--color-primary);
+}
+
+.checkbox-wrapper .switch:checked::after {
+  background-color: #fff;
+  left: 13px;
+}
+
+.checkbox-wrapper :focus:not(.focus-visible) {
+  outline: 0;
+}
+
+.checkbox-wrapper .switch:checked:hover {
+  background-color: var(--color-primary-hover);
+}
+</style>
