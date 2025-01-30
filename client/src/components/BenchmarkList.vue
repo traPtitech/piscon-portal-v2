19cf2b4debcedbab4d24c4f97c355a938acafc8b
cp-20 <n.naoki.1587@gmail.com>
2025-01-30 22:53:44 +0900

    fix view

--- client/src/components/BenchmarkList.vue
+++ client/src/components/BenchmarkList.vue
@@ -46,6 +46,10 @@ const getUserName = (userId: string) => getUserById(userId)?.name ?? ''
         <Icon icon="mdi:calendar-clock" width="24" height="24" />
         <span>日時</span>
       </div>
+      <div class="list-label" v-if="isAdmin">
+        <Icon icon="mdi:account-group" width="24" height="24" />
+        <span>チーム</span>
+      </div>
       <div class="list-label list-server">
         <Icon icon="mdi:server-network" width="24" height="24" />
         <span>対象サーバー</span>
@@ -67,74 +71,30 @@ const getUserName = (userId: string) => getUserById(userId)?.name ?? ''
         <div class="bench-date list-datetime">
           {{ formatDate(bench.createdAt, 'YYYY/MM/DD hh:mm:ss.SSS') }}
         </div>
+        <div v-if="isAdmin" class="bench-team">
+          {{ getTeamName(bench.teamId) }}
+        </div>
         <div class="bench-server list-server">
-          サーバー{{ instances?.find((i) => i.id === bench.instanceId)?.serverId ?? '?' }}
+          サーバー{{ getInstanceServerId(bench.instanceId) }}
         </div>
         <div class="bench-user list-user">
-          <UserAvatar :name="getUserName(bench.userId) ?? ''" />
+          <UserAvatar :name="getUserName(bench.userId)" />
           <span>@{{ getUserName(bench.userId) }}</span>
         </div>
         <div>
           <BenchmarkStatusChip :status="bench.status" />
         </div>
         <div>
-          <RouterLink :to="`/benches/${bench.id}`" class="bench-link">
+          <RouterLink
+            :to="isAdmin ? `/admin/benches/${bench.id}` : `/benches/${bench.id}`"
+            class="bench-link"
+          >
             <span>詳細を見る</span>
             <Icon icon="mdi:chevron-right" width="24" height="24" />
           </RouterLink>
         </div>
       </template>
     </div>
-    <div class="list-label">
-      <Icon icon="mdi:calendar-clock" width="24" height="24" />
-      <span>日時</span>
-    </div>
-    <div class="list-label" v-if="isAdmin">
-      <Icon icon="mdi:account-group" width="24" height="24" />
-      <span>チーム</span>
-    </div>
-    <div class="list-label">
-      <Icon icon="mdi:server-network" width="24" height="24" />
-      <span>対象サーバー</span>
-    </div>
-    <div class="list-label">
-      <Icon icon="mdi:account" width="24" height="24" />
-      <span>実行ユーザー</span>
-    </div>
-    <div class="list-label">
-      <Icon icon="mdi:progress-clock" width="24" height="24" />
-      <span>ステータス</span>
-    </div>
-    <div class="list-label"></div>
-    <template v-for="bench in sortedBenches" :key="bench.id">
-      <div v-if="bench.status === 'running' || bench.status === 'finished'" class="bench-score">
-        {{ formatScore(bench.score) }}
-      </div>
-      <div v-else class="bench-score-loading">計測中</div>
-      <div>
-        {{ formatDate(bench.createdAt, 'YYYY/MM/DD hh:mm:ss.SSS') }}
-      </div>
-      <div v-if="isAdmin" class="bench-team">
-        {{ getTeamName(bench.teamId) }}
-      </div>
-      <div class="bench-server">サーバー{{ getInstanceServerId(bench.instanceId) }}</div>
-      <div class="bench-user">
-        <UserAvatar :name="getUserName(bench.userId)" />
-        <span>@{{ getUserName(bench.userId) }}</span>
-      </div>
-      <div>
-        <BenchmarkStatusChip :status="bench.status" />
-      </div>
-      <div>
-        <RouterLink
-          :to="isAdmin ? `/admin/benches/${bench.id}` : `/benches/${bench.id}`"
-          class="bench-link"
-        >
-          <span>詳細を見る</span>
-          <Icon icon="mdi:chevron-right" width="24" height="24" />
-        </RouterLink>
-      </div>
-    </template>
   </div>
 </template>
 
