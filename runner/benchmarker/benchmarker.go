16d837b7759a7c018f7d0d1e936a9c97f097ee1b
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-02-09 11:57:27 +0900

    :adhesive_bandage: benchmarkerの修正

--- runner/benchmarker/benchmarker.go
+++ runner/benchmarker/benchmarker.go
@@ -17,8 +17,8 @@ type Benchmarker interface {
 	// This mechanism is like [os/exec.Cmd.Start].
 	// If ctx is canceled, the job must be stopped.
 	//
-	// After a successful call to Start the [Executer.Wait] method must be called in order to release associated system resources.
-	Start(ctx context.Context, job *domain.Job) (stdout io.ReadCloser, stderr io.ReadCloser, startedAt time.Time, err error)
+	// After a successful call to Start the [Benchmarker.Wait] method must be called in order to release associated system resources.
+	Start(ctx context.Context, job *domain.Job) (stdout io.Reader, stderr io.Reader, startedAt time.Time, err error)
 	// Wait waits for the job to complete and returns an error if the job failed.
 	Wait(ctx context.Context) (domain.Result, time.Time, error)
 	// CalculateScore calculates the score of the benchmark job based on stdout and stderr content.
