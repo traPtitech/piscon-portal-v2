698775f3bd5d95b4df5eddbe5eab40d41910c739
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-02-13 16:03:10 +0900

    :sparkles: ちょっとDI

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"log"
+	"time"
 
+	portalv1 "github.com/traPtitech/piscon-portal-v2/gen/portal/v1"
 	"github.com/traPtitech/piscon-portal-v2/runner"
+	portalGrpc "github.com/traPtitech/piscon-portal-v2/runner/portal/grpc"
 )
 
 func main() {
-	r := runner.Prepare(nil, nil) //TODO: Implement
+	client := portalv1.NewBenchmarkServiceClient(nil) //TODO: Implement
+	p := portalGrpc.NewPortal(client, time.Second)
+
+	r := runner.Prepare(p, nil) //TODO: Implement
 
 	for {
 		if err := r.Run(); err != nil {
