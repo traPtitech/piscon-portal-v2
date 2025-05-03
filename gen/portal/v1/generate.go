e9774e124540fc202cc30378f90eacc5fb5cc381
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-05-03 17:53:24 +0900

    :factory: ストリーミングサーバーのmock作成

--- gen/portal/v1/generate.go
+++ gen/portal/v1/generate.go
@@ -2,4 +2,4 @@ package portalv1
 
 //go:generate go tool mockgen -package mock -destination mock/benchmark.go -typed . BenchmarkServiceClient
 
-//go:generate go tool mockgen -package mock -destination mock/progress_stream.go -typed google.golang.org/grpc ClientStreamingClient
+//go:generate go tool mockgen -package mock -destination mock/progress_stream.go -typed google.golang.org/grpc ClientStreamingClient,ClientStreamingServer
