47bd696a108f6f94752a6d5fcb37559e6e25265c
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-05-03 17:53:49 +0900

    :sparkles: SendBenchmarkProgressのサーバー実装

--- server/server/benchmark.go
+++ server/server/benchmark.go
@@ -3,10 +3,15 @@ package server
 import (
 	"context"
 	"errors"
+	"io"
 
+	"github.com/google/uuid"
 	portalv1 "github.com/traPtitech/piscon-portal-v2/gen/portal/v1"
+	"github.com/traPtitech/piscon-portal-v2/server/domain"
 	"github.com/traPtitech/piscon-portal-v2/server/usecase"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type BenchmarkService struct {
@@ -41,8 +46,37 @@ func (bs *BenchmarkService) GetBenchmarkJob(ctx context.Context, _ *portalv1.Get
 	}, nil
 }
 
-func (bs *BenchmarkService) SendBenchmarkProgress(grpc.ClientStreamingServer[portalv1.SendBenchmarkProgressRequest, portalv1.SendBenchmarkProgressResponse]) error {
-	// TODO
+func (bs *BenchmarkService) SendBenchmarkProgress(stream grpc.ClientStreamingServer[portalv1.SendBenchmarkProgressRequest, portalv1.SendBenchmarkProgressResponse]) error {
+	ctx := stream.Context()
+	for {
+		req, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return handleError("failed to receive progress", err)
+		}
+
+		benchID, err := uuid.Parse(req.BenchmarkId)
+		if err != nil {
+			return status.Error(codes.InvalidArgument, "invalid benchmark ID")
+		}
+
+		err = bs.b.SaveBenchmarkProgress(ctx, benchID, domain.BenchmarkLog{
+			UserLog:  req.Stdout,
+			AdminLog: req.Stderr,
+		}, req.Score, req.StartedAt.AsTime())
+		if usecase.IsUseCaseError(err) {
+			return status.Error(codes.InvalidArgument, "invalid benchmark status")
+		}
+		if errors.Is(err, usecase.ErrNotFound) {
+			return status.Error(codes.NotFound, "benchmark not found")
+		}
+		if err != nil {
+			return handleError("failed to save benchmark progress", err)
+		}
+	}
+
 	return nil
 }
 func (bs *BenchmarkService) PostJobFinished(context.Context, *portalv1.PostJobFinishedRequest) (*portalv1.PostJobFinishedResponse, error) {
