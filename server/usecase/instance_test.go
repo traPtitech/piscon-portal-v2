e5f3441d73976c18ceaf79067e4807df8a7f4e1c
pirosiki197 <lingmukangtai825@gmail.com>
2025-05-16 00:00:13 +0900

    implement DeleteInstance

--- server/usecase/instance_test.go
+++ server/usecase/instance_test.go
@@ -57,3 +57,36 @@ func TestCreateInstance_tooManyInstances(t *testing.T) {
 	_, err := instanceUseCase.CreateInstance(t.Context(), teamID)
 	assert.ErrorIs(t, err, usecase.NewUseCaseError(domain.ErrInstanceLimitExceeded))
 }
+
+func TestDeleteInstance(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	repo := repomock.NewMockRepository(ctrl)
+	manager := instancemock.NewMockManager(ctrl)
+	usecase := usecase.NewInstanceUseCase(repo, domain.NewInstanceFactory(3), manager)
+
+	instanceID := uuid.New()
+	providerID := uuid.New()
+	instance := domain.Instance{
+		ID: instanceID,
+		Infra: domain.InfraInstance{
+			ProviderInstanceID: providerID.String(),
+			Status:             domain.InstanceStatusDeleted,
+		},
+	}
+
+	repo.EXPECT().Transaction(gomock.Any(), gomock.Any()).DoAndReturn(func(ctx context.Context, f func(context.Context) error) error {
+		return f(ctx)
+	})
+	repo.EXPECT().FindInstance(gomock.Any(), instanceID).Return(instance, nil)
+	manager.EXPECT().Delete(
+		gomock.Any(),
+		gomock.Cond(func(instance domain.InfraInstance) bool {
+			return instance.ProviderInstanceID == providerID.String()
+		}),
+	).Return(domain.InfraInstance{}, nil)
+	repo.EXPECT().UpdateInstance(gomock.Any(), gomock.Any()).Return(nil)
+
+	err := usecase.DeleteInstance(t.Context(), instanceID)
+	assert.NoError(t, err)
+}
