5f2c59ab877abd0d95f73aab844605643872f091
pirosiki <130675136+pirosiki197@users.noreply.github.com>
2025-05-18 15:24:19 +0900

    implement DeleteInstance (#111)
    * implement DeleteInstance
    
    * add a test case

--- server/usecase/instance_test.go
+++ server/usecase/instance_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/traPtitech/piscon-portal-v2/server/domain"
+	"github.com/traPtitech/piscon-portal-v2/server/repository"
 	repomock "github.com/traPtitech/piscon-portal-v2/server/repository/mock"
 	instancemock "github.com/traPtitech/piscon-portal-v2/server/services/instance/mock"
 	"github.com/traPtitech/piscon-portal-v2/server/usecase"
@@ -56,4 +57,55 @@ func TestCreateInstance_tooManyInstances(t *testing.T) {
 
 	_, err := instanceUseCase.CreateInstance(t.Context(), teamID)
 	assert.ErrorIs(t, err, usecase.NewUseCaseError(domain.ErrInstanceLimitExceeded))
+}
+
+func TestDeleteInstance(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	repo := repomock.NewMockRepository(ctrl)
+	manager := instancemock.NewMockManager(ctrl)
+	usecase := usecase.NewInstanceUseCase(repo, domain.NewInstanceFactory(3), manager)
+
+	instanceID := uuid.New()
+	providerID := uuid.New()
+	instance := domain.Instance{
+		ID: instanceID,
+		Infra: domain.InfraInstance{
+			ProviderInstanceID: providerID.String(),
+			Status:             domain.InstanceStatusDeleted,
+		},
+	}
+
+	repo.EXPECT().Transaction(gomock.Any(), gomock.Any()).DoAndReturn(func(ctx context.Context, f func(context.Context) error) error {
+		return f(ctx)
+	})
+	repo.EXPECT().FindInstance(gomock.Any(), instanceID).Return(instance, nil)
+	manager.EXPECT().Delete(
+		gomock.Any(),
+		gomock.Cond(func(instance domain.InfraInstance) bool {
+			return instance.ProviderInstanceID == providerID.String()
+		}),
+	).Return(domain.InfraInstance{}, nil)
+	repo.EXPECT().UpdateInstance(gomock.Any(), gomock.Any()).Return(nil)
+
+	err := usecase.DeleteInstance(t.Context(), instanceID)
+	assert.NoError(t, err)
+}
+
+func TestDeleteInstance_instanceNotFound(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	repo := repomock.NewMockRepository(ctrl)
+	manager := instancemock.NewMockManager(ctrl)
+	instanceUsecase := usecase.NewInstanceUseCase(repo, domain.NewInstanceFactory(3), manager)
+
+	instanceID := uuid.New()
+
+	repo.EXPECT().Transaction(gomock.Any(), gomock.Any()).DoAndReturn(func(ctx context.Context, f func(context.Context) error) error {
+		return f(ctx)
+	})
+	repo.EXPECT().FindInstance(gomock.Any(), instanceID).Return(domain.Instance{}, repository.ErrNotFound)
+
+	err := instanceUsecase.DeleteInstance(t.Context(), instanceID)
+	assert.ErrorIs(t, err, usecase.ErrNotFound)
 }
