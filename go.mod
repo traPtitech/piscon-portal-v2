c131f13b7c791562704f02b64434e5e4e5d5a36c
pirosiki197 <lingmukangtai825@gmail.com>
2025-05-27 18:37:10 +0900

    implement instance manager

--- go.mod
+++ go.mod
@@ -4,6 +4,10 @@ go 1.24
 
 require (
 	github.com/aarondl/opt v0.0.0-20240623220848-083f18ab9536
+	github.com/aws/aws-sdk-go-v2 v1.36.3
+	github.com/aws/aws-sdk-go-v2/config v1.29.14
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.67
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.217.0
 	github.com/coreos/go-oidc/v3 v3.14.1
 	github.com/go-faster/errors v0.7.1
 	github.com/go-faster/jx v1.1.0
@@ -38,6 +42,16 @@ require (
 	github.com/Masterminds/sprig/v3 v3.3.0 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/aarondl/json v0.0.0-20221020222930-8b0db17ef1bf // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.30 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.25.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.30.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.19 // indirect
+	github.com/aws/smithy-go v1.22.2 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/containerd/platforms v0.2.1 // indirect
@@ -60,6 +74,7 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.0.0-alpha.1 // indirect
+	github.com/goccy/go-yaml v1.17.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/gorilla/context v1.1.2 // indirect
---
+++ server/services/instance/aws/aws.go
@@ -0,0 +1,226 @@
+package aws
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/samber/lo"
+	"github.com/traPtitech/piscon-portal-v2/server/domain"
+	"github.com/traPtitech/piscon-portal-v2/server/services/instance"
+)
+
+var _ instance.Manager = (*Client)(nil)
+
+type Client struct {
+	client *ec2.Client
+	cfg    Config
+}
+
+type Config struct {
+	ImageID      string
+	InstanceType string
+	Region       string
+
+	AccessKey string
+	SecretKey string
+
+	SubnetID        string
+	SecurityGroupID string
+	KeyPairName     string
+}
+
+func NewClient(cfg Config) (*Client, error) {
+	awsCfg, err := config.LoadDefaultConfig(context.Background(),
+		config.WithRegion(cfg.Region),
+		config.WithCredentialsProvider(credentials.StaticCredentialsProvider{
+			Value: aws.Credentials{
+				AccessKeyID:     cfg.AccessKey,
+				SecretAccessKey: cfg.SecretKey,
+			},
+		}))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		client: ec2.NewFromConfig(awsCfg),
+		cfg:    cfg,
+	}, nil
+}
+
+func (a *Client) Create(ctx context.Context, name string, sshPubKeys []string) (domain.InfraInstance, error) {
+	tagSpec := types.TagSpecification{
+		ResourceType: types.ResourceTypeInstance,
+		Tags: []types.Tag{
+			{
+				Key:   lo.ToPtr("Name"),
+				Value: &name,
+			},
+			{
+				Key:   lo.ToPtr("piscon"),
+				Value: lo.ToPtr("true"),
+			},
+		},
+	}
+
+	cloudConfig := CloudConfig{
+		Users: []User{
+			{
+				Name:              "isucon",
+				Sudo:              "ALL=(ALL) NOPASSWD:ALL",
+				Groups:            []string{"sudo"},
+				SSHAuthorizedKeys: sshPubKeys,
+			},
+		},
+	}
+	userdata, err := cloudConfig.ConvertToUserData()
+	if err != nil {
+		return domain.InfraInstance{}, fmt.Errorf("failed to generate user data: %w", err)
+	}
+
+	nispec := types.InstanceNetworkInterfaceSpecification{
+		AssociatePublicIpAddress: lo.ToPtr(true),
+		DeleteOnTermination:      lo.ToPtr(true),
+		DeviceIndex:              lo.ToPtr[int32](0),
+		SubnetId:                 &a.cfg.SubnetID,
+		Groups:                   []string{a.cfg.SecurityGroupID},
+	}
+	instanceInput := &ec2.RunInstancesInput{
+		ImageId:           &a.cfg.ImageID,
+		InstanceType:      types.InstanceType(a.cfg.InstanceType),
+		MinCount:          lo.ToPtr[int32](1),
+		MaxCount:          lo.ToPtr[int32](1),
+		TagSpecifications: []types.TagSpecification{tagSpec},
+		NetworkInterfaces: []types.InstanceNetworkInterfaceSpecification{nispec},
+		KeyName:           &a.cfg.KeyPairName,
+		UserData:          lo.ToPtr(userdata),
+	}
+
+	res, err := a.client.RunInstances(ctx, instanceInput)
+	if err != nil {
+		return domain.InfraInstance{}, err
+	}
+
+	instance := res.Instances[0]
+	return domain.InfraInstance{
+		ProviderInstanceID: *instance.InstanceId,
+		PrivateIP:          *instance.PrivateIpAddress,
+		PublicIP:           *instance.PublicIpAddress,
+		Status:             convertInstanceState(instance.State.Name),
+	}, nil
+}
+
+func (a *Client) Delete(ctx context.Context, instance domain.InfraInstance) (domain.InfraInstance, error) {
+	_, err := a.client.TerminateInstances(ctx, &ec2.TerminateInstancesInput{
+		InstanceIds: []string{instance.ProviderInstanceID},
+	})
+	if err != nil {
+		return domain.InfraInstance{}, fmt.Errorf("failed to terminate instance: %w", err)
+	}
+
+	deleted := instance
+	deleted.Status = domain.InstanceStatusDeleting
+
+	return deleted, nil
+}
+
+func (a *Client) Get(ctx context.Context, id string) (domain.InfraInstance, error) {
+	res, err := a.client.DescribeInstances(ctx, &ec2.DescribeInstancesInput{
+		InstanceIds: []string{id},
+	})
+	if err != nil {
+		return domain.InfraInstance{}, fmt.Errorf("failed to describe instance: %w", err)
+	}
+
+	if len(res.Reservations) == 0 || len(res.Reservations[0].Instances) == 0 {
+		return domain.InfraInstance{}, fmt.Errorf("instance not found: %s", id)
+	}
+
+	instance := res.Reservations[0].Instances[0]
+	return domain.InfraInstance{
+		ProviderInstanceID: *instance.InstanceId,
+		PrivateIP:          *instance.PrivateIpAddress,
+		PublicIP:           *instance.PublicIpAddress,
+		Status:             convertInstanceState(instance.State.Name),
+	}, nil
+}
+
+func (a *Client) GetAll(ctx context.Context) ([]domain.InfraInstance, error) {
+	res, err := a.client.DescribeInstances(ctx, &ec2.DescribeInstancesInput{
+		Filters: []types.Filter{
+			{
+				Name:   lo.ToPtr("tag:piscon"),
+				Values: []string{"true"},
+			},
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to describe instances: %w", err)
+	}
+
+	var instances []domain.InfraInstance
+	for _, reservation := range res.Reservations {
+		for _, instance := range reservation.Instances {
+			instances = append(instances, domain.InfraInstance{
+				ProviderInstanceID: *instance.InstanceId,
+				PrivateIP:          *instance.PrivateIpAddress,
+				PublicIP:           *instance.PublicIpAddress,
+				Status:             convertInstanceState(instance.State.Name),
+			})
+		}
+	}
+
+	return instances, nil
+}
+
+func (a *Client) Stop(ctx context.Context, instance domain.InfraInstance) (domain.InfraInstance, error) {
+	_, err := a.client.StopInstances(ctx, &ec2.StopInstancesInput{
+		InstanceIds: []string{instance.ProviderInstanceID},
+	})
+	if err != nil {
+		return domain.InfraInstance{}, fmt.Errorf("failed to stop instance: %w", err)
+	}
+
+	stopped := instance
+	stopped.Status = domain.InstanceStatusStopping
+
+	return stopped, nil
+}
+
+func (a *Client) Start(ctx context.Context, instance domain.InfraInstance) (domain.InfraInstance, error) {
+	_, err := a.client.StartInstances(ctx, &ec2.StartInstancesInput{
+		InstanceIds: []string{instance.ProviderInstanceID},
+	})
+	if err != nil {
+		return domain.InfraInstance{}, fmt.Errorf("failed to start instance: %w", err)
+	}
+
+	started := instance
+	started.Status = domain.InstanceStatusRunning
+
+	return started, nil
+}
+
+func convertInstanceState(state types.InstanceStateName) domain.InstanceStatus {
+	switch state {
+	case types.InstanceStateNamePending:
+		return domain.InstanceStatusPending
+	case types.InstanceStateNameRunning:
+		return domain.InstanceStatusRunning
+	case types.InstanceStateNameStopping:
+		return domain.InstanceStatusStopping
+	case types.InstanceStateNameStopped:
+		return domain.InstanceStatusStopped
+	case types.InstanceStateNameShuttingDown:
+		return domain.InstanceStatusDeleting
+	case types.InstanceStateNameTerminated:
+		return domain.InstanceStatusDeleted
+	default:
+		return domain.InstanceStatusUnknown
+	}
+}
