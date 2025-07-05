409a2fecd41b8f21757f876d93e89cc96c24db85
cp-20 <n.naoki.1587@gmail.com>
2025-07-05 18:40:01 +0900

    🥒 🥐 🍟 ステータスが WAITING / RUNNING のベンチマークの更新をリアルタイムで行うように

--- client/src/components/TeamBenchmarkList.vue
+++ client/src/components/TeamBenchmarkList.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
 import BenchmarkList from '@/components/BenchmarkList.vue'
 import { useTeamBenches, useTeamInstances } from '@/lib/useServerData'
+import { onMounted, onUnmounted } from 'vue'
 
 const { teamId } = defineProps<{ teamId: string }>()
 
-const { data: benches, error: benchesError } = useTeamBenches(teamId)
+const { data: benches, error: benchesError, refetch } = useTeamBenches(teamId)
 const { data: instances } = useTeamInstances(teamId)
+
+let interval: number
+onMounted(() => {
+  interval = setInterval(() => {
+    const loadingStatuses = ['waiting', 'running']
+    if (benches.value?.some((b) => loadingStatuses.includes(b.status))) {
+      void refetch()
+    }
+  }, 500)
+})
+
+onUnmounted(() => {
+  clearInterval(interval)
+})
 </script>
 
 <template>
