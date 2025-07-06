d9d165b5f99568e8d09e574a5b87c00b6b66e50a
cp-20 <n.naoki.1587@gmail.com>
2025-07-07 00:34:18 +0900

    format

--- client/src/components/TeamBenchmarkDetail.vue
+++ client/src/components/TeamBenchmarkDetail.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
-import { useEnqueueBenchmark, useTeamBench, useTeamBenches, useTeamInstances } from '@/lib/useServerData'
+import {
+  useEnqueueBenchmark,
+  useTeamBench,
+  useTeamBenches,
+  useTeamInstances,
+} from '@/lib/useServerData'
 import { computed, watch } from 'vue'
 
 const { teamId, benchId } = defineProps<{
@@ -37,23 +42,32 @@ watch(
 
 <template>
   <div class="team-bench-detail-container">
-    <MainButton @click="reEnqueueBenchmark" class="bench-re-enqueue-button" :disabled="!canReEnqueue" v-tooltip.bottom="!canReEnqueue ? {
-      value: '現在実行中のベンチマークがあるため、新しいベンチマークを実行できません',
-      pt: {
-        arrow: {
-          style: {
-            borderBottomColor: 'rgba(var(--ct-slate-800-rgb), 0.9)',
-          }
-        },
-        text: {
-          style: {
-            color: 'var(--ct-slate-100)',
-            backgroundColor: 'rgba(var(--ct-slate-800-rgb), 0.9)',
-            fontSize: '0.875rem',
-          }
-        }
-      }
-    } : ''">
+    <MainButton
+      @click="reEnqueueBenchmark"
+      class="bench-re-enqueue-button"
+      :disabled="!canReEnqueue"
+      v-tooltip.bottom="
+        !canReEnqueue
+          ? {
+              value: '現在実行中のベンチマークがあるため、新しいベンチマークを実行できません',
+              pt: {
+                arrow: {
+                  style: {
+                    borderBottomColor: 'rgba(var(--ct-slate-800-rgb), 0.9)',
+                  },
+                },
+                text: {
+                  style: {
+                    color: 'var(--ct-slate-100)',
+                    backgroundColor: 'rgba(var(--ct-slate-800-rgb), 0.9)',
+                    fontSize: '0.875rem',
+                  },
+                },
+              },
+            }
+          : ''
+      "
+    >
       <Icon icon="mdi:thunder" width="20" height="20" />
       <span>ベンチマーク再実行</span>
     </MainButton>
