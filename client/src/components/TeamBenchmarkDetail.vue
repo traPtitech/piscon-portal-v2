148e9bf946e0f4f58b052c68ed9a4ae8dcd8f0f8
cp-20 <n.naoki.1587@gmail.com>
2025-07-07 00:30:38 +0900

    🥧 🥖 🍗 ベンチマーク詳細画面で実行中のベンチマークが既にあるときの再実行を許可しないように

--- client/src/components/TeamBenchmarkDetail.vue
+++ client/src/components/TeamBenchmarkDetail.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
-import { useEnqueueBenchmark, useTeamBench, useTeamInstances } from '@/lib/useServerData'
-import { watch } from 'vue'
+import { useEnqueueBenchmark, useTeamBench, useTeamBenches, useTeamInstances } from '@/lib/useServerData'
+import { computed, watch } from 'vue'
 
 const { teamId, benchId } = defineProps<{
   teamId: string
@@ -9,8 +9,11 @@ const { teamId, benchId } = defineProps<{
 }>()
 
 const { data: bench, error: benchError, refetch } = useTeamBench(teamId, benchId)
+const { data: benches } = useTeamBenches(teamId)
 const { mutate: enqueueBenchmark } = useEnqueueBenchmark({ redirect: true })
 const { data: instances } = useTeamInstances(teamId)
+
+const canReEnqueue = computed(() => benches.value?.every((b) => b.status === 'finished'))
 
 const reEnqueueBenchmark = () => {
   if (bench.value === undefined) return
@@ -34,11 +37,23 @@ watch(
 
 <template>
   <div class="team-bench-detail-container">
-    <MainButton
-      @click="reEnqueueBenchmark"
-      class="bench-re-enqueue-button"
-      :disabled="bench?.status === 'running' || bench?.status === 'waiting'"
-    >
+    <MainButton @click="reEnqueueBenchmark" class="bench-re-enqueue-button" :disabled="!canReEnqueue" v-tooltip.bottom="!canReEnqueue ? {
+      value: '現在実行中のベンチマークがあるため、新しいベンチマークを実行できません',
+      pt: {
+        arrow: {
+          style: {
+            borderBottomColor: 'rgba(var(--ct-slate-800-rgb), 0.9)',
+          }
+        },
+        text: {
+          style: {
+            color: 'var(--ct-slate-100)',
+            backgroundColor: 'rgba(var(--ct-slate-800-rgb), 0.9)',
+            fontSize: '0.875rem',
+          }
+        }
+      }
+    } : ''">
       <Icon icon="mdi:thunder" width="20" height="20" />
       <span>ベンチマーク再実行</span>
     </MainButton>
