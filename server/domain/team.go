9d56e6ab697b4d34ed7c2678d6cd77ed3085d0c9
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2026-08-14 16:33:19 +0900

    :fire: AddMemberの削除

--- server/domain/team.go
+++ server/domain/team.go
@@ -2,7 +2,6 @@ package domain
 
 import (
 	"errors"
-	"slices"
 	"time"
 
 	"github.com/google/uuid"
@@ -39,20 +38,5 @@ func (t *Team) SetMembers(users []User) error {
 	}
 
 	t.Members = users
-	return nil
-}
-
-func (t *Team) AddMember(user User) error {
-	if slices.ContainsFunc(t.Members, func(u User) bool { return u.ID == user.ID }) {
-		return nil
-	}
-	if user.TeamID.Valid && user.TeamID.UUID != t.ID {
-		return errors.New("user is already in another team")
-	}
-	if len(t.Members) >= MaxTeamMembers {
-		return errors.New("team is full")
-	}
-	user.TeamID = uuid.NullUUID{UUID: t.ID, Valid: true}
-	t.Members = append(t.Members, user)
 	return nil
 }
